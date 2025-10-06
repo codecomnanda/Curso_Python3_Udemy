3bc79313dd74b03e3b127dfabb8189547f80a401
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-10-06 19:21:48 -0300

    Create aula22_op_not.py

---
+++ aula22_op_not.py
@@ -0,0 +1,11 @@
+# Operador lógico NOT
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+# senha = input('Senha: ')
+# if senha != '123456':
+#    print('Acesso Negado')
+
+print(not True)
+print(not False)
