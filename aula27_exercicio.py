9aa6cf48cb1e731b872c230e90c5d5cc39d6aede
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-10-07 18:31:21 -0300

    Update aula27_exercicio.py

--- aula27_exercicio.py
+++ aula27_exercicio.py
@@ -16,13 +16,13 @@ nome = input('Digite o seu nome: ')
 idade = input('Digite a sua idade: ')
 if nome and idade:
     print(f'Seu nome é {nome}')
-    print(f'Seu nome invertido é {nome[::-1]}')
-    if ' ' in nome:
+    print(f'Seu nome invertido é {nome[::-1]}') # Invertendo a string
+    if ' ' in nome: # Verificando se tem espaço no nome
         print('Seu nome contém espaços')
     else:
         print('Seu nome não contém espaços')
-    print(f'Seu nome tem {len(nome)} letras')
-    print(f'A primeira letra do seu nome é {nome[0]}')
-    print(f'A última letra do seu nome é {nome[-1]}')
+    print(f'Seu nome tem {len(nome)} letras') # Contando a quantidade de letras no nome
+    print(f'A primeira letra do seu nome é {nome[0]}') # Pegando a primeira letra do nome
+    print(f'A última letra do seu nome é {nome[-1]}') # Pegando a última letra do nome
 else:
     print('Desculpe, você deixou algum campo vazio.')
