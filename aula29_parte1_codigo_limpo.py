d05d75c4c84ff374b070170651f3e284f9981baf
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-10-07 20:07:53 -0300

    Update aula29_parte1_codigo_limpo.py

--- aula29_parte1_codigo_limpo.py
+++ aula29_parte1_codigo_limpo.py
@@ -14,3 +14,16 @@ RADAR_1 = 60 # Velocidade máxima do radar 1
 LOCAL_1 = 100 # Local onde o radar 1 está
 RADAR_RANGE = 1 # A distância onde o radar pega
 
+vel_carro_passou_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 + RADAR_RANGE)
+local_carro <= (LOCAL_1 + RADAR_RANGE) # local_carro <= 101
+carro_multado_radar_1 = vel_carro_passou_radar_1 and carro_passou_radar_1
+
+if vel_carro_passou_radar_1:
+    print('Velocidade do carro passou do radar 1')
+
+if carro_passou_radar_1:
+    print('Carro passou no radar 1')
+
+if carro_multado_radar_1:
+    print('Carro multado no radar 1')
