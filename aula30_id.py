7304a9aed19f9ea3a47c888d33245a311a144577
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-10-07 20:26:38 -0300

    Aula 30 id

---
+++ aula30_id.py
@@ -0,0 +1,6 @@
+'''
+Flag (Bandeira) - Marcar um local no código para ser usado posteriormente
+None = Nulo, nada, vazio, indefinido
+is e is not = é ou não é (compara o valor e o tipo)
+id = Identidade do objeto (onde está na memória)
+'''
