2b56143fac3055d4c81dc662a2977272d41b8747
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-10-07 19:17:38 -0300

    Condições para um código mais limpo

---
+++ aula29_parte1_codigo_limpo.py
@@ -0,0 +1,16 @@
+'''
+CONSTANTE = "Varriáveis" que não mudam de valor
+Muitas condições no mesmo if (ruim para a legibilidade)
+    <- Contagem de complexidade (ruim para a legibilidade)
+
+Simples é melhor que complexo
+Complexo é melhor que complicado
+'''
+
+velocidade = 61 # Velocidade atual do carro
+local_carro = 100 # Local onde o carro está
+
+RADAR_1 = 60 # Velocidade máxima do radar 1
+LOCAL_1 = 100 # Local onde o radar 1 está
+RADAR_RANGE = 1 # A distância onde o radar pega
+
