75b6a921866a4f4cb3c07ac0f5ff1b93d5834f56
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-10-07 20:30:41 -0300

    Update aula30_id.py

--- aula30_id.py
+++ aula30_id.py
@@ -4,11 +4,17 @@ None = Nulo, nada, vazio, indefinido
 is e is not = é ou não é (compara o valor e o tipo)
 id = Identidade do objeto (onde está na memória)
 '''
-
+'''
 v1 = 'a'
 v2 = 'b'
 
 print(id(v1))
 print(id(v2))
+'''
 
-# condicao =
+condicao = False
+
+if condicao:
+    print('Faça algo')
+else:
+    print('Não faça algo')
