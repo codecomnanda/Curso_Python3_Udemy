4636f9348fc440e3a189142a64f58454594b077d
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-10-10 16:06:31 -0300

    Update aula34_while_break.py

--- aula34_while_break.py
+++ aula34_while_break.py
@@ -6,8 +6,20 @@ loop infinito -> código que nunca termina
 break (interrompe)
 '''
 
+'''
 contador = 0
 while contador < 10:
     contador = contador + 1
     print(contador)
 print('Acabou!')
+'''
+
+'''
+Operadores de atribuição no while
+= += -= *= /= //= **= %=
+'''
+
+contador = 0
+while contador < 10:
+    contador += 1 # contador = contador + 1
+    print(contador)
