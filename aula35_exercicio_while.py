3f08190642f744821a8a2b44bb6a4c11a77b3cdc
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-10-10 17:06:42 -0300

    Create aula35_exercicio_while.py

---
+++ aula35_exercicio_while.py
@@ -0,0 +1,17 @@
+'''
+Iterando strings com while
+
+nome = 'Fernanda Guimarães' # Iteráveis
+tamanho_nome = len(nome) # len() -> função que retorna o tamanho de uma string
+print(nome)
+print(tamanho_nome)
+print(nome[3]) 
+
+'''
+nome = 'Fernanda Guimarães'
+tamanho_nome = len(nome) # len() -> função que retorna o tamanho de uma string
+indice = 0
+while indice < tamanho_nome: # enquanto indice for menor que o tamanho do nome
+    print(f'*{nome[indice]}', end='') # end='' -> para não pular linha
+    indice += 1 # incrementa o índice
+print('*')
