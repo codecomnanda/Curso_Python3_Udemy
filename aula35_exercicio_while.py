89f1f545ec1bbf854d437e329f27746a7e2a0d08
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-10-23 16:56:54 -0300

    Update aula35_exercicio_while.py

--- aula35_exercicio_while.py
+++ aula35_exercicio_while.py
@@ -13,5 +13,5 @@ tamanho_nome = len(nome) # len() -> função que retorna o tamanho de uma string
 indice = 0
 while indice < tamanho_nome: # enquanto indice for menor que o tamanho do nome
     print(f'*{nome[indice]}', end='') # end='' -> para não pular linha
-    indice += 1 # incrementa o índice
+    indice += 1 # incrementa o índice, ou seja, soma +1 a cada iteração
 print('*')
