4da49005dd3d9b1812ce6582c1ca31768c4f155d
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-11-05 19:57:34 -0300

    Subindo

---
+++ aula38_while_letra_apareceu_mais.py
@@ -0,0 +1,24 @@
+
+frase = 'O Python é uma linguagem de programação' \
+'multiparadgma. ' \
+'Python foi criado por Guido Van Rossum.'
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qta_apareceu_mais_vezes_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qta_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = qta_apareceu_mais_vezes_atual
+        letra_apareceu_mais_vezes = letra_atual
+    i += 1
+
+print(f'A letra que apareceu mais vezes foi a "{letra_apareceu_mais_vezes}", '
+      f'que apareceu {qtd_apareceu_mais_vezes} vezes.')
