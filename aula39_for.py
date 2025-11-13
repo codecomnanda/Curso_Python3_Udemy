314e7428fc3853750d6871e1a44ac57861057dc7
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-11-13 14:16:14 -0300

    For

---
+++ aula39_for.py
@@ -0,0 +1,5 @@
+texto = 'Python'
+for letra in texto:
+    print(letra)
+
+# a variável 'letra' recebe cada caractere da string 'texto' a cada iteração do loop. Eu crio essa variável com um nome que faça sentido para o contexto do loop.
