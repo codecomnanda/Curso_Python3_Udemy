190196415128eab2f014a458f6a846c62b38f721
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2025-11-13 14:35:06 -0300

    For + Range

---
+++ aula40_range_for.py
@@ -0,0 +1,7 @@
+'''For + range
+
+range -> range(start, stop, step)'''    
+
+numeros = range(1, 10, 2)  # start=1, stop=10, step=2, ou seja, começa em 1, vai até 10 (não incluindo o 10) e conta de 2 em 2
+for numero in numeros: # para cada número dentro de números, exiba:
+    print(numero)
